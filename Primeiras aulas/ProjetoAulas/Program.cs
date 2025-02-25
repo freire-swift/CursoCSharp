2eabd4ee48f842b5061d41b9ab8113cc2c027c9d
Pedro Henrique Freire de Oliveira <pedrobolina123@gmail.com>
2025-02-25 17:18:33 -0300

    Fundamentos C# fim do hands on code

--- Primeiras aulas/ProjetoAulas/Program.cs
+++ Primeiras aulas/ProjetoAulas/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Console.WriteLine("Primeiro programa");
+
+// int numero = 1;
+// Console.WriteLine(numero);
+// string nome = "Pedro";
+// decimal valor = 200.99m;
+// double valorDouble = 200.99;
+// float valorFloat = 0.9f;
+// var idadeNova = 34;
+// char flag = 'Y';
+// Console.WriteLine(flag);
+// bool ativo = false;
+
+// const string descricao = "Curso C#";
+// Console.WriteLine(descricao);
+
+var numero1 = 1;
+var numero2 = 2;
+
+// var soma = numero1 + numero2;
+// Console.WriteLine(soma);
+
+// var subtracao = numero2 - numero1;
+// Console.WriteLine(subtracao);
+
+// var multiplicacao = (numero1 * numero2) * 10;
+// Console.WriteLine(multiplicacao);
+
+// var divisao = numero2 / numero1;
+// Console.WriteLine(divisao);
+
+// bool igual = numero1 == numero2;
+// Console.WriteLine(igual);
+
+// bool maior = numero2 > numero1;
+// Console.WriteLine(maior);
+
+// bool menor = numero2 < numero1;
+// Console.WriteLine(menor);
+
+// bool menorOuIgual = numero2 <= numero1;
+// Console.WriteLine(menorOuIgual);
+
+// bool maiorOuIgual = numero2 >= numero1;
+// Console.WriteLine(maiorOuIgual);
+
+// bool diferente = numero2 != numero1;
+// Console.WriteLine(diferente);
+
+// var valido = numero2 > numero1 && 8 > 7;
+// Console.WriteLine(valido);
+
+// var valido2 = numero2 > numero1 || 6 > 7;
+// Console.WriteLine(valido);
+
+// var valido3 = !(numero2 > 10);
+// Console.WriteLine(valido3);
+
+bool ativo = true;
+string status = !ativo ? "Cadastro Ativo" : "Cadastro inativo";
+Console.WriteLine(status);
